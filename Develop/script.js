e27f51dcfb439361692b92b1b8f332444f5d4ab9
Leandro Parrado <lparrado321@gmail.com>
2020-12-18 01:42:57 -0500

    Added code to function getPassWordOptions()

--- Develop/script.js
+++ Develop/script.js
@@ -7,8 +7,35 @@ function writePassword() {
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
 
+  function generatePassword() {
+    var options = getPasswordOptions();
+  }
+
+  // Retrieves password
+  function getPasswordOptions() {
+
+    confirm("Minimum characters for password is 8.");
+    var length = parseInt(prompt("Please enter password desired: "));
+
+    if length >= 8 && length <= 128 {
+      var hasSpecialCharacters = confirm("Click ok to confirm special characters.");
+
+      var random = math.floor(Math.random() * length);
+
+      return hasSpecialCharacters[random];
+    }
+
+    else {
+      confirm("Invalid number of characters.");
+    }
+  }
+
+  // Conditional statement to ensure user input
+
+  //Check password length
+
+  var hasSpecialCharacters() = confirm()
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
